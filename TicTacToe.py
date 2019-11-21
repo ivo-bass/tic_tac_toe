152166c38d2d712c3c37648a8d903925f7aaa646
Alexander Ignatov <ani1802@abv.bg>
2019-11-21 15:01:27 +0200

    Refactor code to remove nested functions, global variables and to organize loops

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,135 +1,129 @@
-# ____Global Variables____
-board = ['_', '_', '_',
-         '_', '_', '_',
-         '_', '_', '_']
-game_still_going = True
-winner = None
-current_player = "X"
+#!/usr/bin/python3
 
 
-def start():
-    prompt = input("Play a game? |y/n|: ")
+def display_board(board):
+    print("\n")
+    print("|" + board[0] + "|" + board[1] + "|" + board[2] + "|" + "      |1|2|3|")
+    print("|" + board[3] + "|" + board[4] + "|" + board[5] + "|" + "      |4|5|6|")
+    print("|" + board[6] + "|" + board[7] + "|" + board[8] + "|" + "      |7|8|9|")
+    print("\n")
 
-    while prompt.lower() == "y":
 
-        def display_board():
-            print("\n")
-            print("|" + board[0] + "|" + board[1] + "|" + board[2] + "|" + "      |1|2|3|")
-            print("|" + board[3] + "|" + board[4] + "|" + board[5] + "|" + "      |4|5|6|")
-            print("|" + board[6] + "|" + board[7] + "|" + board[8] + "|" + "      |7|8|9|")
-            print("\n")
+def play_game():
+    # Game setup
+    board = [
+        '_', '_', '_',
+        '_', '_', '_',
+        '_', '_', '_'
+    ]
+    current_player = "X"
+    display_board(board)
 
-        def play_game():
-            display_board()
-            while game_still_going:
+    # Game loop
+    while True:
+        handle_turn(board, current_player)
+        if game_is_over(board): break
+        current_player = flipped_player(current_player)
 
-                handle_turn(current_player)
+    # Game over
+    winner = get_winner_if_available(board)
+    if winner == "X" or winner == "O":
+        print("\n" + str(winner) + " won!")
+    elif winner is None:
+        print("It's a TIE.")
 
-                check_if_game_over()
 
-                flip_player()
-
-            if winner == "X" or winner == "O":
-                print("\n" + str(winner) + " won!")
-                start()
-            elif winner is None:
-                print("It's a TIE.")
-                start()
-
-        def handle_turn(player):
-            print(player + "'s turn now.")
+def handle_turn(board, player):
+    print(player + "'s turn now.")
+    position = input("Choose a position from 1 to 9: ")
+    valid = False
+    while not valid:
+        while position not in [str(n) for n in range(1, 10)]:  # note to bro: това се нарича list comprehension, което е причината да обичам python :D
             position = input("Choose a position from 1 to 9: ")
-            valid = False
-            while not valid:
-                while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-                    position = input("Choose a position from 1 to 9: ")
-                position = int(position) - 1
-                if board[position] == "_":
-                    valid = True
-                else:
-                    print("You can't go there. Go again.")
-            board[position] = player
-            display_board()
+        position = int(position) - 1
+        if board[position] == "_":
+            valid = True
+        else:
+            print("You can't go there. Go again.")
+    board[position] = player
+    display_board(board)
 
-        def check_if_game_over():
-            check_for_winner()
-            check_if_tie()
 
-        def check_for_winner():
-            global winner
-            row_winner = check_rows()
-            column_winner = check_columns()
-            diagonal_winner = check_diagonals()
-            if row_winner:
-                winner = row_winner
-            elif column_winner:
-                winner = column_winner
-            elif diagonal_winner:
-                winner = diagonal_winner
-            else:
-                winner = None
-            return
+def game_is_over(board):
+    winner = get_winner_if_available(board)
+    is_full = board_is_full(board)
+    return winner is not None or is_full
 
-        def check_rows():
-            global game_still_going
-            row_1 = board[0] == board[1] == board[2] != "_"
-            row_2 = board[3] == board[4] == board[5] != "_"
-            row_3 = board[6] == board[7] == board[8] != "_"
-            if row_1 or row_2 or row_3:
-                game_still_going = False
-            if row_1:
-                return board[0]
-            if row_2:
-                return board[3]
-            if row_3:
-                return board[6]
-            return
 
-        def check_columns():
-            global game_still_going
-            column_1 = board[0] == board[3] == board[6] != "_"
-            column_2 = board[1] == board[4] == board[7] != "_"
-            column_3 = board[2] == board[5] == board[8] != "_"
-            if column_1 or column_2 or column_3:
-                game_still_going = False
-            if column_1:
-                return board[0]
-            if column_2:
-                return board[1]
-            if column_3:
-                return board[2]
-            return
+def get_winner_if_available(board):
+    row_winner = check_rows(board)
+    column_winner = check_columns(board)
+    diagonal_winner = check_diagonals(board)
+    if row_winner:
+        return row_winner
+    elif column_winner:
+        return column_winner
+    elif diagonal_winner:
+        return diagonal_winner
+    else:
+        return None
 
-        def check_diagonals():
-            global game_still_going
-            diagonal_1 = board[0] == board[4] == board[8] != "_"
-            diagonal_2 = board[6] == board[4] == board[2] != "_"
-            if diagonal_1 or diagonal_2:
-                game_still_going = False
-            if diagonal_1:
-                return board[0]
-            if diagonal_2:
-                return board[6]
-            return
 
-        def check_if_tie():
-            global game_still_going
-            if "_" not in board:
-                game_still_going = False
-            return
+def check_rows(board):
+    row_1 = board[0] == board[1] == board[2] != "_"
+    row_2 = board[3] == board[4] == board[5] != "_"
+    row_3 = board[6] == board[7] == board[8] != "_"
+    if row_1:
+        return board[0]
+    if row_2:
+        return board[3]
+    if row_3:
+        return board[6]
+    return None
 
-        def flip_player():
-            global current_player
-            if current_player == "X":
-                current_player = "O"
-            elif current_player == "O":
-                current_player = "X"
-            return
 
+def check_columns(board):
+    column_1 = board[0] == board[3] == board[6] != "_"
+    column_2 = board[1] == board[4] == board[7] != "_"
+    column_3 = board[2] == board[5] == board[8] != "_"
+    if column_1:
+        return board[0]
+    if column_2:
+        return board[1]
+    if column_3:
+        return board[2]
+    return None
+
+
+def check_diagonals(board):
+    diagonal_1 = board[0] == board[4] == board[8] != "_"
+    diagonal_2 = board[6] == board[4] == board[2] != "_"
+    if diagonal_1:
+        return board[0]
+    if diagonal_2:
+        return board[6]
+    return None
+
+
+def board_is_full(board):
+    return "_" not in board
+
+
+def flipped_player(player):
+    if player == "X":
+        return "O"
+    elif player == "O":
+        return "X"
+
+
+def main():
+    while True:
+        prompt = input("Start another game? [y/n]: ")
+        game_is_requested = prompt.lower() in ('y', 'yes', 'da', 'aidi')
+        if not game_is_requested: break
         play_game()
-        break
 
+    input("Press any key to quit the game.")
 
-start()
-
-quit_game = input("Press any key to quit the game.")
+if __name__ == "__main__":  # Това нещо ще го виждаш често, забранява на следващия код да се изпълнява, когато файлът бива import-нат в друг; изъплнява се само когато рънваме текущия файл
+    main()
